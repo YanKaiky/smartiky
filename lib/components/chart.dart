fe0c76dcb3651988fa5ac02b9f710adb772bea22
Yan Kaiky <yankaikys@gmail.com>
2023-12-27 21:51:44 -0300

    feat(Chart): remove chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -2,9 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:smartiky/models/transactions.history/transactions.history.model.dart';
 import 'package:smartiky/repositories/transactions.history.repository.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
-import 'package:smartiky/utils/colors.dart';
 
 class Chart extends StatefulWidget {
   final int index;
@@ -19,9 +16,7 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
-  List<TransactionsHistoryModel> a = [];
-  bool b = true;
-  bool j = true;
+  List<TransactionsHistoryModel> data = [];
 
   final TransactionsHistoryRepository transactionsHistoryRepository =
       TransactionsHistoryRepository();
@@ -36,35 +31,11 @@ class _ChartState extends State<Chart> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return const SizedBox(
       width: double.infinity,
       height: 300,
-      child: SfCartesianChart(
-        primaryXAxis: CategoryAxis(
-          labelStyle: const TextStyle(
-            color: AppColors.text,
-          ),
-        ),
-        primaryYAxis: NumericAxis(
-          labelStyle: const TextStyle(
-            color: AppColors.text,
-          ),
-        ),
-        series: <SplineSeries<SalesData, String>>[
-          SplineSeries<SalesData, String>(
-            color: AppColors.primaryBlack,
-            width: 3,
-            dataSource: <SalesData>[
-              SalesData(100, 'Mon'),
-              SalesData(20, 'Tue'),
-              SalesData(40, 'Wen'),
-              SalesData(15, 'Sat'),
-              SalesData(5, 'Sun'),
-            ],
-            xValueMapper: (SalesData sales, _) => sales.month,
-            yValueMapper: (SalesData sales, _) => sales.sales,
-          ),
-        ],
+      child: Center(
+        child: Text('Chart'),
       ),
     );
   }
@@ -72,33 +43,24 @@ class _ChartState extends State<Chart> {
   Future getTransactionsHistoryPeriod() async {
     switch (widget.index) {
       case 0:
-        a = await TransactionsHistoryRepository()
+        data = await TransactionsHistoryRepository()
             .getTransactionsHistoryPeriod();
-        b = true;
-        j = true;
         break;
       case 1:
-        a = await TransactionsHistoryRepository()
+        data = await TransactionsHistoryRepository()
             .getTransactionsHistoryPeriod(period: 'week');
-        b = false;
-        j = true;
         break;
       case 2:
-        a = await TransactionsHistoryRepository()
+        data = await TransactionsHistoryRepository()
             .getTransactionsHistoryPeriod(period: 'month');
-        b = false;
-        j = true;
         break;
       case 3:
-        a = await TransactionsHistoryRepository()
+        data = await TransactionsHistoryRepository()
             .getTransactionsHistoryPeriod(period: 'year');
-        j = false;
         break;
       default:
-        a = await TransactionsHistoryRepository()
+        data = await TransactionsHistoryRepository()
             .getTransactionsHistoryPeriod();
-        b = true;
-        j = true;
     }
 
     if (mounted) setState(() {});
